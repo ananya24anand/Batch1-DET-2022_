046223d99b566c41be5bcd9589cb5403dc5223cc
Etg1 <Etg1@69J0RG2-SHEL>
2022-08-30 14:25:26 +0530

    changes

---
+++ Batch1-DET-2022_/CSFeatures.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022_
+{
+    internal class CSFeatures
+    {
+        public static void Main()
+        {
+            Action<string> action = (string name) => { Console.WriteLine($"Hi {name}" + $" Type is {typeof(Action)}"); };
+
+            Action<string, string> action1 = (string s1, string s2) => { Console.WriteLine($"Hi {s1}, " + $"you said {s2}"); };
+
+            //action.Invoke("Ananya");
+            //action1.Invoke("Ananya", "am in Blr");
+
+
+            Func<int, int, long> multiply = (x, y) => { return x * y; };
+            long result = multiply(1, 2);
+            Console.WriteLine(result);
+
+        }
+
+        
+    }
+}
---
+++ Batch1-DET-2022_/ICarPrice.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Batch1_DET_2022_
+{
+    internal interface ICarPrice
+    {
+        int GetPrice(string name);
+    }
+
+    class BMW : ICarPrice
+    {
+        public string Name { get; set; }
+        public int GetPrice(string name)
+        {
+            if (name == "M3")
+                return 13000000;
+            else if (name == "X7")
+                return 1000000;
+            else
+                return 9000000;
+        }
+
+    }
+
+    class Hundai : ICarPrice
+    {
+        public string Name { get; set; }
+        public int GetPrice(string name)
+        {
+            if (name == "Creta")
+                return 1000000;
+            else if (name == "tucson")
+                return 2500000;
+            else
+                return 1000000;
+        }
+    }
+
+    class TestICar
+    {
+        public static void Main()
+        {
+            BMW a = new BMW { Name = "bmw" };
+            Hundai sa = new Hundai { Name = "hundai" };
+
+            Console.WriteLine(a.GetPrice("M3"));
+
+            Console.WriteLine(sa.GetPrice("santro"));
+        }
+    }
+}
